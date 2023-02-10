6ab48967a4aac678669dc47f99b7455df39606a0
Qesait <kuzva2003@gmail.com>
2023-02-10 13:16:03 +0300

    Верстка главной страницы

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,12 +1,58 @@
-import {ButtonComponent} from "../../components/button/index.js";
+import {ProductCardComponent} from "../../components/product-card/index.js";
 
 export class MainPage {
     constructor(parent) {
         this.parent = parent;
     }
-    
+
+    get pageRoot() {
+        return document.getElementById('main-page')
+    }
+        
+    getHTML() {
+        return (
+            `
+                <div id="main-page" class="d-flex flex-wrap"><div/>
+            `
+        )
+    }
+        
+    getData() {
+        return [
+            {
+                id: 1,
+                src: "Тюлень.jpg",
+                title: "Акция",
+                text: "Такой акции вы еще не видели 1"
+            },
+            {
+                id: 2,
+                src: "Тюлень.jpg",
+                title: "Акция",
+                text: "Такой акции вы еще не видели 2"
+            },
+            {
+                id: 3,
+                src: "Тюлень.jpg",
+                title: "Акция",
+                text: "Такой акции вы еще не видели 3"
+            },
+        ]
+    }
+
+    clickCard(e) {
+        const cardId = e.target.dataset.id
+    }
+        
     render() {
-        const button = new ButtonComponent(this.parent)
-        button.render()
+        this.parent.innerHTML = ''
+        const html = this.getHTML()
+        this.parent.insertAdjacentHTML('beforeend', html)
+        
+        const data = this.getData()
+        data.forEach((item) => {
+            const productCard = new ProductCardComponent(this.pageRoot)
+            productCard.render(item, this.clickCard.bind(this))
+        })
     }
 }
